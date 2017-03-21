4713144bd8fd9fe579b5006f454a04145da7ab97
Anatoliy Klots and Peter Karellas <anatoliy@>
2017-03-21 11:30:16 -0700

    add buttons and java script

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("button#yellow").click(function() {
+     $("body").removeClass();
+    $("body").addClass("light");
+    });
+
+    $("button#blue").click(function() {
+       $("body").removeClass();
+      $("body").addClass("dark");
+      });
+});
+  // $(".click").click(function() {
+  //   $(".showing").toggle();
+  //   $(".hidden").toggle();
+  // });
